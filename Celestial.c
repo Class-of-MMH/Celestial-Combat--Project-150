24dc31c8697eb9e0be996cf32dd331a6e2e7ac7a
Abdullah Al Mamun <137890438+mamun2021331086@users.noreply.github.com>
2023-12-29 23:35:55 +0600

    Update Celestial1.c

--- Celestial.c
+++ Celestial.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
+#include <time.h>
+#include<windows.h>
+int G[26][76],X[452],Y[452],planeMove,bombY,bombX,bombOn=0;
+
+void Plane(){
+    G[planeMove][3] = 1;
+
+    G[planeMove+1][3] = 1;
+    G[planeMove+1][4] = 1;
+
+    G[planeMove+2][3] = 1;
+    G[planeMove+2][4] = 1;
+    G[planeMove+2][5] = 1;
+
+    G[planeMove+3][3] = 1;
+    G[planeMove+3][4] = 1;
+
+    G[planeMove+4][3] = 1;
+}
 
